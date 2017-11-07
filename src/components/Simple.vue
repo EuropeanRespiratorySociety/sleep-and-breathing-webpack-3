1fe2f6e41e00c31971a982aad7c76d86c54f38e4
idealley <samuelpouyt@gmail.com>
2017-11-07 11:01:32 +0100

    Added requests to API and rendering

--- src/components/Simple.vue
+++ src/components/Simple.vue
@@ -4,7 +4,7 @@
       <v-flex xs12 sm12 md10 lg10 offset-md1 offset-lg1>
         <v-card id="test" class="card--flex-toolbar">
           <v-toolbar card class="white">
-            <v-toolbar-title class="body-2 grey--text">Title</v-toolbar-title>
+            <v-toolbar-title class="body-2 grey--text">{{category.title}}</v-toolbar-title>
             <v-spacer></v-spacer>
             <v-btn icon>
               <v-icon>search</v-icon>
@@ -17,102 +17,24 @@
             </v-btn>
           </v-toolbar>
 
-        <v-tabs dark fixed centered>
-          <v-tabs-bar class="cyan">
-            <v-tabs-slider color="yellow"></v-tabs-slider>
-            <v-tabs-item
-              v-for="i in 3"
-              :key="i"
-              :href="'#tab-' + i"
-            >
-              Item {{ i }}
-            </v-tabs-item>
-          </v-tabs-bar>
-          <v-tabs-items>
-            <v-tabs-content
-              v-for="i in 3"
-              :key="i"
-              :id="'tab-' + i"
-            >
-              <v-card flat>
-                <v-card-text>{{ text }}</v-card-text>
-              </v-card>
-            </v-tabs-content>
-          </v-tabs-items>
-        </v-tabs> 
-
-        <v-divider></v-divider>
-        <v-card-text>text </v-card-text>
-        <v-container grid-list-md text-xs-center>
-          <v-layout row wrap>
-
-            <v-flex xs12 sm6>
-                <v-card>
-                  <v-card-media src="https://cdn.pixabay.com/photo/2017/09/22/23/57/fire-2777580_960_720.jpg" height="200px">
-                  </v-card-media>
-                  <v-card-title primary-title>
-                    <div>
-                      <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-                      <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
-                    </div>
-                  </v-card-title>
-                  <v-card-actions>
-                    <v-btn flat color="orange">Share</v-btn>
-                    <v-btn flat color="orange">Explore</v-btn>
-                  </v-card-actions>
-                </v-card>
-            </v-flex>
-
-            <v-flex xs12 sm6>
-              <v-card>
-                <v-card-media src="https://cdn.pixabay.com/photo/2017/09/22/23/57/fire-2777580_960_720.jpg" height="200px">
-                </v-card-media>
-                <v-card-title primary-title>
-                  <div>
-                    <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-                    <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
-                  </div>
-                </v-card-title>
-                <v-card-actions>
-                  <v-btn flat color="orange">Share</v-btn>
-                  <v-btn flat color="orange">Explore</v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
+          <v-divider></v-divider>
+          <v-card-text v-html="category.body"></v-card-text>
         </v-card>
 
         <v-container grid-list-md>
-          <v-layout row wrap>
-            <v-flex xs12 sm6>
+          <v-layout v-if="posts && posts.length" row wrap>
+            <v-flex v-for="post of posts" xs12 sm6>
               <v-card>
-                <v-card-media src="https://cdn.pixabay.com/photo/2017/09/22/23/57/fire-2777580_960_720.jpg" height="200px">
+                <v-card-media :src="post.image" height="200px">
                 </v-card-media>
                 <v-card-title primary-title>
                   <div>
-                    <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-                    <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+                    <h3 class="headline mb-0">{{post.title}}</h3>
+                    <div v-html="post.leadParagraph"></div>
                   </div>
                 </v-card-title>
                 <v-card-actions>
-                  <v-btn flat color="orange">Share</v-btn>
-                  <v-btn flat color="orange">Explore</v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-flex>
-            <v-flex xs12 sm6>
-              <v-card>
-                <v-card-media src="https://cdn.pixabay.com/photo/2017/09/22/23/57/fire-2777580_960_720.jpg" height="200px">
-                </v-card-media>
-                <v-card-title primary-title>
-                  <div>
-                    <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-                    <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
-                  </div>
-                </v-card-title>
-                <v-card-actions>
-                  <v-btn flat color="orange">Share</v-btn>
+                  <v-btn :href="`/${post.slug}`" flat color="orange">More...</v-btn>
                   <v-btn flat color="orange">Explore</v-btn>
                 </v-card-actions>
               </v-card>
@@ -126,14 +48,28 @@
 </template>
 
 <script>
+  import axios from 'axios'
+
   export default {
     name: 'Simple',
     data () {
       return {
         fixed: false,
-        title: 'Vuetify.js',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        category: {},
+        posts: [],
+        errors: []
       }
+    },
+    // Fire a request when the component is created.
+    created () {
+      axios.get(`https://api.ersnet.org/news`)
+      .then(response => {
+        this.posts = response.data.data
+        this.category = response.data.category[0]
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
     }
   }
 </script>
