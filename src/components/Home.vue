ab2d295a8ad25c1c0071afb99cf01b08886e3e72
tasanoers <tamaki.asano@ersnet.org>
2017-11-24 16:32:28 +0100

    Modify Home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,5 @@
 <template>
+
   <v-flex xs12 sm12 md8 lg8 offset-md2 offset-lg2>
     <v-card id="test" class="card--flex-toolbar">
       <v-toolbar card color="white">
@@ -10,6 +11,36 @@
       <v-card-text v-if="category" v-html="categoryContent"></v-card-text>
     </v-card>
     
+    <v-container grid-list-md>
+    <v-layout row wrap>
+      <v-flex xs12 sm6>
+        <v-card id="test" class="card--flex-toolbar">
+          <v-toolbar card color="white">
+            <v-toolbar-title v-if="category" class="headline grey--text">{{category.title}}</v-toolbar-title>
+            <v-spacer></v-spacer>
+          </v-toolbar>
+
+          <v-divider></v-divider>
+          <v-card-text v-if="category" v-html="categoryContent"></v-card-text>
+        </v-card>
+      </v-flex>
+
+      <v-flex xs12 sm6>
+        <v-card id="test" class="card--flex-toolbar">
+          <v-toolbar card color="white">
+            <v-toolbar-title v-if="category" class="headline grey--text">{{category.title}}</v-toolbar-title>
+            <v-spacer></v-spacer>
+          </v-toolbar>
+
+          <v-divider></v-divider>
+          <v-card-text v-if="category" v-html="categoryContent"></v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    </v-container>
+
+
+
     <v-container grid-list-md>
       <v-layout v-if="articles" row wrap>
         <v-flex v-for="post of articles" xs12 sm6 :key="post.slug">
@@ -31,14 +62,62 @@
         </v-flex>
       </v-layout>
     </v-container>
-    <ads></ads>
+
+    <v-layout row wrap>
+      <v-flex xs12 sm6>
+        <v-card color="yellow darken-3" class="white--text" href="http://www.ers-education.org/publications/handbook-series/ers-handbook-of-respiratory-sleep-medicine.aspx">
+          <v-container fluid grid-list-lg>
+            <v-layout row>
+              <v-flex xs8>
+                <div>
+                  <div class="headline">Sleep Medicine Textbook</div>
+                  <p class="grey--text text--darken-2">Endorsed by ERS</p>
+                  <p class="grey--text text--darken-3">Based on the <a target="_blank" href="http://onlinelibrary.wiley.com/doi/10.1111/jsr.12095/abstract">
+                    Catalogue of knowledge and skills for sleep medicine
+                  </a></p>
+                </div>
+              </v-flex>
+              <v-flex xs4>
+                <v-card-media
+                  src="../../static/img/ESRS-Sleep-Medicine-Textbook_150.jpg"
+                  height="215px"
+                  contain
+                ></v-card-media>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 sm6>
+        <v-card color="yellow darken-3" class="white--text" href="http://www.ers-education.org/publications/handbook-series/ers-handbook-of-respiratory-sleep-medicine.aspx">
+          <v-container fluid grid-list-lg>
+            <v-layout row>
+              <v-flex xs8>
+                <div>
+                  <div class="headline">ERS Handbook of respiratory sleep medicine</div>
+                  <p class="grey--text text--darken-2">Endorsed by ESRS</p>
+                  <p class="grey--text text--darken-3">Accredited by European Board for Accreditation in Pneumology (EBAP)</p>
+                </div>
+              </v-flex>
+              <v-flex xs4>
+                <v-card-media
+                  src="../../static/img/sleep-handbook.jpg"
+                  height="215px"
+                  contain
+                ></v-card-media>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
     </v-flex>
 </template>
 
 <script>
   import { mapActions } from 'vuex'
-  import Ads from './widgets/Ads'
   // import * as config from '../../config'
+
   export default {
     name: 'Home',
     data () {
@@ -75,6 +154,7 @@
             class="sponsors"
           >`
       }
+
     },
 
     methods: {
@@ -91,8 +171,7 @@
         }
         this.getCategory(payload)
       }
-    },
-    components: { Ads }
+    }
 
   }
 </script>
